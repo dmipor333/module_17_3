7afb147bd1f3c5f2a7ed8d08c6ae322eda451123
dmipor333 <dmipor333@ya.ru>
2024-12-03 02:37:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.routers import task, user
+
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanadger"}
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+
+# uvicorn app.main:app --reload
